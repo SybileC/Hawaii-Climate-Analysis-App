7622bff4950e6f6d26a1fd2762d9440c9ac20c3a
SybileC <sybile.cherenfant@gmail.com>
2020-10-21 02:37:02 -0400

    Made changes to code

--- app.py
+++ app.py
@@ -37,8 +37,19 @@ def home():
 @app.route("/api/v1.0/precipitation")
 def precipitation():
 
-data = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date > year).all()
+    data = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date > year).all()
 
+    session.close()
+
+    precipitation = []
+    for prcp in results:
+        precipitation_dict = {}
+        passenger_dict["name"] = name
+        passenger_dict["age"] = age
+        passenger_dict["sex"] = sex
+        precipitation.append(precipitation_dict)
+
+    return jsonify(precipitation_dict)
 
 
 
