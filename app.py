089546db676d97386f1a681a72ca03b356346f41
SybileC <sybile.cherenfant@gmail.com>
2020-10-21 01:53:40 -0400

    Made changes to code

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
-from flask import Flask
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
 
 # Create an app
 app = Flask(__name__)
@@ -7,5 +15,17 @@ app = Flask(__name__)
 # 3. Define what to do when a user hits the index route
 @app.route("/")
 def home():
+    routes = ['precipitation', 'stations', 'tobs', 'start']
+    return routes
+
+@app.route("/precipitation")
+def precipitation():
+    return
+
+
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
